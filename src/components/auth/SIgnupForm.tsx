1f6a803032e497c2eedc03592fef02dd0edda817
Artur Oliveira <64819062+aetussilicon@users.noreply.github.com>
2025-04-17 23:18:47 -0300

    ✨ fix(signup): corrigir ícone de exibição de senha de confirmação

--- src/components/auth/SIgnupForm.tsx
+++ src/components/auth/SIgnupForm.tsx
@@ -57,7 +57,7 @@ const SignupForm = () => {
                     className='absolute right-3 top-2.5 text-gray-500'
                     onClick={() => setShowConfirmPassword(!showConfirmPassword)}
                 >
-                    {showPassword ? <EyeOff /> : <Eye />}
+                    {showConfirmPassword ? <EyeOff /> : <Eye />}
                 </button>
             </div>
         </>
