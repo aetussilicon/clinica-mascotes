dbdc433ead6cf2b82a5248e339cb9ea5def440be
Artur Oliveira <64819062+aetussilicon@users.noreply.github.com>
2025-04-18 12:01:36 -0300

    ✨ feat(profile): criar tela de perfil do usuário
    - Adiciona a tela de perfil do usuário com informações pessoais e endereço
    - Implementa componente `UserInfo` para exibir os dados do usuário
    - Estiliza a página com layout responsivo e design moderno
    - Adiciona botões para editar o perfil e navegar para outras seções
    - Inclui placeholders para imagem de perfil e dados do usuário

---
+++ src/components/profile/UserInfo.tsx
@@ -0,0 +1,49 @@
+import { MapPinned } from "lucide-react";
+
+const UserInfo = ({ user }: { user?: any } = {}) => {
+    const TextField = ({ label, value }: { label: string; value: string }) => {
+        return (
+            <div className='flex flex-col gap-1 border max-w-full sm:max-w-[20rem] px-4 py-2 rounded-lg'>
+                <label className='text-sm font-semibold text-gray-600 capitalize' htmlFor='name'>
+                    {label}
+                </label>
+                <input
+                    type='text'
+                    id='name'
+                    value={value}
+                    readOnly
+                    className='bg-transparent text-gray-800'
+                />
+            </div>
+        );
+    };
+
+    return (
+        <div className='p-4'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+                <TextField label='Nome Completo' value={user?.fullName} />
+                <TextField label='Contato' value={user?.phone} />
+                <TextField label='data de nascimento' value={user?.birthDate} />
+                <TextField label='email' value={user?.email} />
+                <TextField label='CPF' value={user?.cpf} />
+                <TextField label='RG' value={user?.rg} />
+            </div>
+            <div>
+                <h1 className='flex gap-2 text-2xl font-bold items-center text-primary my-4'>
+                    <MapPinned size={28} />
+                    Endereço
+                </h1>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+                    <TextField label='Rua' value={user?.address?.street} />
+                    <TextField label='Número' value={user?.address?.number} />
+                    <TextField label='Bairro' value={user?.address?.district} />
+                    <TextField label='Cidade' value={user?.address?.city} />
+                    <TextField label='Estado' value={user?.address?.state} />
+                    <TextField label='CEP' value={user?.address?.postalCode} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserInfo;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,6 @@
-import { Camera, PencilLine } from 'lucide-react';
+import { Camera, PawPrint, PencilLine, ShoppingBag, User, UserIcon } from 'lucide-react';
 import Layout from '../components/Layout';
+import UserInfo from '../components/profile/UserInfo';
 
 export function Profile() {
     const user = {
@@ -12,6 +13,7 @@ export function Profile() {
             district: 'Jd. Irapiranga',
             street: 'Rua das Flores',
             number: '123',
+            postalCode: '12345-678',
             complement: 'Apto 101',
         },
         cpf: '123.456.789-00',
@@ -21,28 +23,52 @@ export function Profile() {
 
     return (
         <Layout>
-            <div className='flex items-center justify-center h-screen'>
-                <div className='flex flex-col gap-4'>
-                    <h1 className='text-3xl font-bold'>Meu Perfil</h1>
-                    <div className='flex flex-col min-w-[22rem] items-center border-2 p-6 rounded-lg gap-4'>
-                        <div className='relative cursor-pointer'>
-                            <img
-                                src='profile-placeholder.jpg'
-                                alt='Profile'
-                                className='rounded-full w-32 h-32 border-4 border-primaryHover'
-                            />
-                            <div className='absolute bg-primary p-1 rounded-full text-white bottom-2 right-1'>
-                                <Camera size={22} />
+            <div className='flex items-center justify-center min-h-screen p-4'>
+                <div className='flex flex-col gap-4 w-full max-w-6xl'>
+                    <h1 className='text-2xl md:text-3xl font-bold text-center md:text-left'>Meu Perfil</h1>
+                    <div className='flex flex-col lg:flex-row gap-6'>
+                        <div className='flex flex-col items-center border-2 p-6 rounded-lg gap-4 w-full lg:w-1/3 h-fit'>
+                            <div className='relative cursor-pointer'>
+                                <img
+                                    src='profile-placeholder.jpg'
+                                    alt='Profile'
+                                    className='rounded-full w-24 h-24 md:w-32 md:h-32 border-4 border-primaryHover'
+                                />
+                                <div className='absolute bg-primary p-1 rounded-full text-white bottom-2 right-1'>
+                                    <Camera size={20} />
+                                </div>
+                            </div>
+                            <div className='text-center'>
+                                <h1 className='text-lg md:text-xl font-bold'>{user.fullName}</h1>
+                                <p className='text-sm md:text-lg text-gray-600'>{user.email}</p>
+                            </div>
+                            <button className='border border-primary text-primary rounded-md w-full py-2 flex items-center justify-center gap-2 hover:bg-primary hover:text-white transition'>
+                                <PencilLine className='w-5 h-5 mr-2' />
+                                <span>Editar perfil</span>
+                            </button>
+                        </div>
+                        <div className='w-full lg:w-2/3'>
+                            <div className='bg-[#f1f5f9] py-2 rounded-lg flex items-center justify-center gap-4 flex-wrap'>
+                                {['Informações', 'Pedidos', 'Meus Pets'].map((text, index) => (
+                                    <button
+                                        key={index}
+                                        className='flex items-center gap-2 px-4 py-1 hover:bg-gray-200 rounded-md transition'
+                                    >
+                                        {text === 'Informações' && <PencilLine className='w-5 h-5' />}
+                                        {text === 'Pedidos' && <ShoppingBag className='w-5 h-5' />}
+                                        {text === 'Meus Pets' && <PawPrint className='w-5 h-5' />}
+                                        <span className='text-sm font-medium'>{text}</span>
+                                    </button>
+                                ))}
+                            </div>
+                            <div className='border border-gray-300 rounded-lg p-4 mt-4 flex flex-col gap-4'>
+                                <h1 className='flex gap-2 text-xl md:text-2xl font-bold items-center text-primary'>
+                                    <User size={24} />
+                                    Dados pessoais
+                                </h1>
+                                <UserInfo user={user} />
                             </div>
                         </div>
-                        <div className='text-center'>
-                            <h1 className='text-xl font-bold'>{user.fullName}</h1>
-                            <p className='text-lg text-gray-600'>{user.email}</p>
-                        </div>
-                        <button className='border border-primary text-primary rounded-md w-full py-2 flex items-center justify-center gap-2 hover:bg-primary hover:text-white transition'>
-                            <PencilLine className='w-5 h-5 mr-2' /> 
-                            <span>Editar perfil</span>
-                        </button>
                     </div>
                 </div>
             </div>
